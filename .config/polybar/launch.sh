9469e0ad9f9d075f810e288b98f4051e99d5e4a7
bryant-the-coder <foobryant99@gmail.com>
2022-10-21 19:16:09 +0800

    change somethings in polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,3 +5,10 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 polybar main
+#!/usr/bin/env bash
+# killall -q polybar
+
+# while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# polybar --reload top &
+# polybar --reload bottom &
